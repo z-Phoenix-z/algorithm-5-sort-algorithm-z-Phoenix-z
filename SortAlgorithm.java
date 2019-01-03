fc789188e1e8668c38c3a1a6f1df7baa9144fe2e
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2019-01-03 13:41:32 +0800

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -1,3 +1,19 @@
 public class SortAlgorithm{
-
+  public SelectionSort {
+        int[] a = { 3, 8, 1, 10 };
+        for (int i = 0; i < a.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[index]) {
+                    index = j;
+                }
+            }
+            int tep = a[i];
+            a[i] = a[index];
+            a[index] = tep;
+        }
+        for (int i : a)
+            System.out.println(i);
+    }
+  
 }
