12c71e539a6bc7f773a973b57c003a95a673ef1e
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2019-01-04 13:57:57 +0800

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -15,93 +15,93 @@ public class SortAlgorithm{
         for (int i : a)
             System.out.println(i);
     }
-  public BucketSort{
-  int [] a = new int [2,3,4,5,2,7];
-  int [] b = new int [7+1];               //长度等于a的最大值，看条件写不写找maximum的方法。 最大值要➕1；
-  for (int i = 0; i < a.length ; i ++){
-   b [a[i]] ++;
-  }
-  for (int i = 0; i < b.length; i ++){
-   if (b [i] != 0){
-      System.out.print( b[i] );
-   }
-  } 
+    ___________________________________________________________________________________________________
+    public BucketSort{
+    int [] a = new int [2,3,4,5,2,7];
+    int [] b = new int [7+1];               //长度等于a的最大值，看条件写不写找maximum的方法。 最大值要➕1；
+    for (int i = 0; i < a.length ; i ++){
+    b [a[i]] ++;
+    }
+    for (int i = 0; i < b.length; i ++){
+    if (b [i] != 0){
+        System.out.print( b[i] );
+    }
+    } 
+    ___________________________________________________________________________________________________
     public BubbleSort {
     int [] a = {5,4,2,1,3};
-int n = 1;
-while (n < a.length){
-  for(int i = 0;i < a.length-n; i ++){
-    int b = 0;
-    if (a[i]<a[i+1]){
-      b = a[i];
-      a[i] = a[i+1];
-      a[i+1] = b;
+    int n = 1;
+    while (n < a.length){
+      for(int i = 0;i < a.length-n; i ++){
+        int b = 0;
+        if (a[i]<a[i+1]){
+          b = a[i];
+          a[i] = a[i+1];
+          a[i+1] = b;
+        }
+        n++;
+      }
     }
-n++;
- }
-for (int i = 0 ; i < a.length; i ++){
-  System.out.print(a[i]);
-}
-}
-      public class MergeSort {
-    public static void mergeSort(int[] ints, int leftStart, int rightEnd) {
+   for (int i = 0 ; i < a.length; i ++){
+     System.out.print(a[i]);
+   }
+  }
+    ___________________________________________________________________________________________________
+    //overloading
+    public static void mergeSort(int[] ints){
+        int start = 0;
+        int end = ints.length - 1;
+        mergeSort(ints, start, end);
+    }
+    private static void mergeSort(int[] ints,int leftStart, int rightEnd) {
         // base case
         if (leftStart == rightEnd) {
             return;
         }
-        int leftEnd = (rightEnd - leftStart) / 2 + leftStart;
+        // sort left half
+        int leftEnd = (rightEnd - leftStart)/2 + leftStart;
         mergeSort(ints, leftStart, leftEnd);
         mergeSort(ints, leftEnd + 1, rightEnd);
         merge(ints, leftStart, leftEnd, rightEnd);
     }
 
     public static void merge(int[] ints, int leftStart, int leftEnd, int rightEnd) {
-        int[] temp = new int[rightEnd - leftStart + 1];
+        int[] tmp = new int[rightEnd - leftStart + 1];
         int l = leftStart;
         int r = leftEnd + 1;
         int index = 0;
         while (l <= leftEnd && r <= rightEnd) {
             if (ints[l] < ints[r]) {
-                temp[index] = ints[l];
+                tmp[index] = ints[l];
                 index++;
                 l++;
-            }
-            if (ints[r] < ints[l]) {
-                temp[index] = ints[r];
+            } else if (ints[r] < ints[l]) {
+                tmp[index] = ints[r];
                 index++;
                 r++;
             } else {
-                temp[index] = ints[l]; // 另一种情况就是俩数相等，就随便找个数组放进去一个。
+                tmp[index] = ints[l];
                 index++;
                 l++;
             }
         }
         while (l <= leftEnd) {
-            temp[index] = ints[l];
+            tmp[index] = ints[l];
             index++;
             l++;
         }
         while (r <= rightEnd) {
-            temp[index] = ints[r];
+            tmp[index] = ints[r];
             index++;
             r++;
         }
-        for (int i = 0; i < temp.length; i++) {
-            ints[leftStart + i] = temp[i];
+        for (int i = 0; i < tmp.length; i++){
+            ints[leftStart + i] = tmp[i];
         }
     }
-
-    public static void print(int[] ints) {
-        for (int a : ints) {
-            System.out.println(a + " ");
-        }
-    }
-
     public static void main(String[] args) {
-        int[] ints = { 1, 3, -1, 5 };
-        // 0 1 2 3
-        merge(ints, 0, 1, 3);
-        print(ints);
+        int[] ints = ArrayUtils.randomArray(100, 100);
+        mergeSort(ints);
+        ArrayUtils.print(ints);
     }
-}
 }
